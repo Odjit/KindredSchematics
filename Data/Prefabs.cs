ee08b4f010c9cad9abbb4900c19e663c7110834c
Odjit <odjiti@gmail.com>
2024-06-06 21:29:21 -0400

    Missed prefabs

--- Data/Prefabs.cs
+++ Data/Prefabs.cs
@@ -4,6 +4,7 @@ using Stunlock.Core;
 namespace KindredVignettes.Data;
 public static class Prefabs
 {
+    public static readonly PrefabGUID External_Inventory = new(1183666186);
     public static readonly PrefabGUID ImprisonedBuff = new(1603329680);
     public static readonly PrefabGUID TM_Castle_Wall_Tier02_Stone_EntranceCrown = new(-251630856);
     public static readonly PrefabGUID TM_Castle_Wall_Tier02_Stone_Window = new(-764944520);
