bd30db760f3015d6c1a92c2a740355fc87304d38
Odjit <odjiti@gmail.com>
2024-08-24 22:05:13 -0400

    Excluding prefab names from search that don't actually exist

--- Data/Tile.cs
+++ Data/Tile.cs
@@ -11,9 +11,29 @@ internal static class Tile
         {
             if (!name.StartsWith("TM_")) continue;
 
+
             // Verify the prefab actually exists
-            if (!Core.PrefabCollection._PrefabGuidToEntityMap.TryGetValue(prefabGuid, out var _))
+            if (!Core.PrefabCollection._PrefabGuidToEntityMap.TryGetValue(prefabGuid, out var _) &&
+                !Core.PrefabCollection._PrefabLookupMap.TryGetValue(prefabGuid, out var _))
+            {
                 continue;
+            }
+
+            Named[name] = prefabGuid;
+            NameFromPrefab[prefabGuid.GuidHash] = name;
+            LowerCaseNameToPrefab[name.ToLower()] = prefabGuid;
+        }
+
+        foreach (var (name, prefabGuid) in Core.PrefabCollection._SpawnableNameToPrefabGuidDictionary)
+        {
+            if (!name.StartsWith("TM_")) continue;
+
+            // Verify the prefab actually exists
+            if (!Core.PrefabCollection._PrefabGuidToEntityMap.TryGetValue(prefabGuid, out var _) &&
+                !Core.PrefabCollection._PrefabLookupMap.TryGetValue(prefabGuid, out var _))
+            {
+                continue;
+            }
 
             Named[name] = prefabGuid;
             NameFromPrefab[prefabGuid.GuidHash] = name;
